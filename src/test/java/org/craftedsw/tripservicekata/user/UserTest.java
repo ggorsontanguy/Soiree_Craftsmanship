772beb088cac16fc0924fa115d51f951f4afe26e
Guillaume Gorson-Tanguy <guillaume@gorson-tanguy.net>
2016-02-08 09:23:43 +0100

    Create Test repertories

---
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -0,0 +1,11 @@
+package org.craftedsw.tripservicekata.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.craftedsw.tripservicekata.trip.Trip;
+
+public class UserTest{
+
+	
+}
