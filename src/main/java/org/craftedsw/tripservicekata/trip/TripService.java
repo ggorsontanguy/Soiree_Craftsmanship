0abb5b0a11bf3ebcc485b1da2f24199f890feac2
Guillaume Gorson-Tanguy <guillaume@gorson-tanguy.net>
2016-02-08 09:19:15 +0100

    Add Maven + Rename TripService_Original

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,15 +1,13 @@
-package org.craftedsw.tripservicekata;
+package org.craftedsw.tripservicekata.trip;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
-import org.craftedsw.tripservicekata.trip.Trip;
-import org.craftedsw.tripservicekata.trip.TripDAO;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
-public class TripService_Original {
+public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
