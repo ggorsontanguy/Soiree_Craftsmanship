0345170451f4c23ce2616d0acfbb6c89f386cd23
Guillaume Gorson-Tanguy <guillaume@gorson-tanguy.net>
2016-02-09 20:24:42 +0100

    java 8 + TDD RED should_raise_exception_when_no_user_logged

--- src/test/java/org/craftedsw/tripservicekata/user/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/user/TripServiceTest.java
@@ -1,12 +1,28 @@
 package org.craftedsw.tripservicekata.user;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.trip.TripService;
 import org.junit.Test;
 
 public class TripServiceTest {
-	// tu devrais installer moreunit => ok  
+
+	// tu devrais installer moreunit => ok
+
 	@Test
-	public void test() {
-		
+	public void should_raise_exception_when_no_user_logged() {
+		TripService tripservice = new TripService();
+
+		assertThatThrownBy(() -> {
+			tripservice.getTripsByUser(null);
+		}).isInstanceOf(UserNotLoggedInException.class);
+
 	}
+
+	// @Test
+	// public void should_behavior_when_scenario() {
+	//
+	// }
 
 }
