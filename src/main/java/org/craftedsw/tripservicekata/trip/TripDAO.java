9c8d92851abfbe62be272120b0930c320aed31e3
Guillaume Gorson-Tanguy <guillaume@gorson-tanguy.net>
2016-02-09 21:25:16 +0100

    Introduce Instance Delegator

--- src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
@@ -7,9 +7,16 @@ import org.craftedsw.tripservicekata.user.User;
 
 public class TripDAO {
 
+	// Introduce Instance Delegator
 	public static List<Trip> findTripsByUser(User user) {
 		throw new CollaboratorCallException(
 				"TripDAO should not be invoked on an unit test.");
 	}
+	
+	public List<Trip> retrieveripsByUser(User user) {
+		return findTripsByUser(user);
+	}
+	
+	
 	
 }
