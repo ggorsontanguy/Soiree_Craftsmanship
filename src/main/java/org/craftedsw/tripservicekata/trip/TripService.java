761b5deee672a164258512a61d45a9ded0774756
Guillaume Gorson-Tanguy <guillaume@gorson-tanguy.net>
2016-02-09 21:48:50 +0100

    REFACTO

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,6 +1,7 @@
 package org.craftedsw.tripservicekata.trip;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
@@ -10,32 +11,28 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	TripDAO tripDAO;
-	
+
 	// Parametrized Constructor
 	TripService() {
-		this (new TripDAO());
+		this(new TripDAO());
 	}
-	
+
 	TripService(TripDAO tripDAO) {
 		this.tripDAO = tripDAO;
 	}
-	
+
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		
+
 		// Extract & Override Call
 		User loggedUser = getLoggedUser();
 		if (loggedUser == null) {
 			throw new UserNotLoggedInException();
 		}
-		
+
 		// Feature Envy
 		boolean isFriend = user.isFriendWith(loggedUser);
-		
-		if (isFriend) {
-			return tripDAO.retrieveripsByUser(user);
-		}
-		return new ArrayList<Trip>();
 
+		return isFriend ? tripDAO.retrieveripsByUser(user) : Collections.emptyList();
 	}
 
 	User getLoggedUser() {
